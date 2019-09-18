04b76792496ab7d1d9ac268ce153d888fee92fbd
andre <andre.alvarez@ucsp.edu.pe>
2019-09-17 21:30:29 -0500

    Update threads.cpp

--- threads.cpp
+++ threads.cpp
@@ -122,3 +122,134 @@ int main()
 		cout << vec[i]<<" ";
 	
 }
+-------------------------------------------------------------------------
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include<stdlib.h>
+
+using namespace std;
+
+void merge(vector<long>& vec, long start, long mid, long end)
+{
+
+	/*std::sort(vec.begin() + start, vec.begin() + mid + 1);
+	std::sort(vec.begin() + mid + 1, vec.begin() + end + 1);*/
+	vector<int> one(vec.begin() + start, vec.begin() + mid + 1);
+	vector<int> two(vec.begin() + mid + 1, vec.begin() + end + 1);
+
+	int a = 0;
+	int b = 0;
+	int index = start;
+	while (a < one.size() && b < two.size())
+	{
+		if (one[a] < two[b])
+			vec[index++] = one[a++];
+		else
+			vec[index++] = two[b++];
+	}
+	
+
+	while (a < one.size())
+		vec[index++] = one[a++];
+	while (b < two.size())
+		vec[index++] = two[b++];
+}
+
+void merge_sort(vector<long>& vec, long start, long end)
+{
+	if (start >= end)
+		return;
+
+
+	int mid = start + (end - start) / 2;
+	
+	thread first(merge_sort, std::ref(vec), start, mid);	
+	thread second(merge_sort, std::ref(vec), mid + 1, end);
+	first.join();
+	second.join();
+	merge(vec, start, mid, end);
+}
+
+
+void procesador(int a, int& nt)
+{
+	if (a == 0)
+		return;
+	while ((a < nt))
+		nt /= 2;
+	
+}
+
+
+void rellenar(int tam, int* ptr)
+{
+	srand(time(NULL));
+	for (int c = 0; c < tam; c++)
+	{
+		int num = 1 + rand() % (11 - 1);
+		*(ptr + c) = num;
+	}
+	cout << "La matriz generada:";
+	for (int i = 0; i < tam; i++)
+	{
+
+		cout << ptr[i] << " ";
+	}
+	cout << "\n\n\n";
+}
+int main()
+{
+	int nt = thread::hardware_concurrency();
+	int tam,tam2=0,size3,size2;
+	int temp = 0,temp2;
+
+	cout << "ingrese el tamaño de su array: ";
+	cin >> tam;
+
+	int size = tam;
+	int* a = new int[tam];
+	size2 = size / nt;
+	
+	rellenar(tam, a);
+	
+	vector<long> vec(a, a + size + 1);
+
+	procesador(size, nt);
+	while (size2 > 1500)
+		size2 = size2 / nt;
+	//cout << size2;
+	size3 = size2;
+	temp2 = size3;
+	
+	while (temp <=nt)
+	{
+		while (size2 <= size)
+		{
+			merge_sort(vec, tam2, size2);
+			tam2 = size2;
+			size2 += size3;
+			
+		}
+		
+		temp++;
+		tam2 = 0;
+		size3 = size2;
+		size2 = temp2*temp;
+		cout << size2<< " ";
+		if (size2 >= 1499)
+		{
+			temp = 0;
+			break;
+		}
+			
+		//size2 = size3;
+	}
+	if (temp == 0)
+		std::sort(vec.begin(), vec.begin() + size+ 1);
+	for (int i = 0; i < size - 2; i++)
+	cout << vec[i] << " ";
+	
+
+}
