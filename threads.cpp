72db46e994b17f01176b2a4022478e6551a96098
andre <andre.alvarez@ucsp.edu.pe>
2019-09-19 17:33:05 -0500

    Update threads.cpp

--- threads.cpp
+++ threads.cpp
@@ -133,9 +133,6 @@ using namespace std;
 
 void merge(vector<long>& vec, long start, long mid, long end)
 {
-
-	/*std::sort(vec.begin() + start, vec.begin() + mid + 1);
-	std::sort(vec.begin() + mid + 1, vec.begin() + end + 1);*/
 	vector<int> one(vec.begin() + start, vec.begin() + mid + 1);
 	vector<int> two(vec.begin() + mid + 1, vec.begin() + end + 1);
 
@@ -143,13 +140,7 @@ void merge(vector<long>& vec, long start, long mid, long end)
 	int b = 0;
 	int index = start;
 	while (a < one.size() && b < two.size())
-	{
-		if (one[a] < two[b])
-			vec[index++] = one[a++];
-		else
-			vec[index++] = two[b++];
-	}
-	
+		(one[a] < two[b])? vec[index++] = one[a++]: vec[index++] = two[b++];
 
 	while (a < one.size())
 		vec[index++] = one[a++];
@@ -165,13 +156,12 @@ void merge_sort(vector<long>& vec, long start, long end)
 	int mid = start + (end - start) / 2;
 	for (int i = 0; i < 1; i++)
 	{
-		t[i]=thread(merge_sort, std::ref(vec), start, mid);
-		t[i+1]=thread(merge_sort, std::ref(vec), mid + 1, end);
+		t[i] = thread(merge_sort, std::ref(vec), start, mid);
+		t[i + 1] = thread(merge_sort, std::ref(vec), mid + 1, end);
 	}
 	for (int i = 0; i < 2; i++)
 		t[i].join();
 	merge(vec, start, mid, end);
-	
 }
 
 
@@ -181,7 +171,6 @@ void procesador(int a, int& nt)
 		return;
 	while ((a < nt))
 		nt /= 2;
-	
 }
 
 
@@ -201,11 +190,20 @@ void rellenar(int tam, int* ptr)
 	}
 	cout << "\n\n\n";
 }
+int valides(vector<long>& vec, long size)
+{
+	if (size <= 7 && size > 1)
+	{
+		merge_sort(vec, 0, size);
+		return 0;
+	}
+	else return 0;
+}
 int main()
 {
 	int nt = thread::hardware_concurrency();
-	int tam,tam2=0,size3,size2;
-	int temp = 0,temp2;
+	int tam, tam2 = 0, size3, size2;
+	int temp = 0, temp2,validez=0;
 
 	cout << "ingrese el tamaño de su array: ";
 	cin >> tam;
@@ -213,9 +211,9 @@ int main()
 	int size = tam;
 	int* a = new int[tam];
 	size2 = size / nt;
-	
+
 	rellenar(tam, a);
-	
+
 	vector<long> vec(a, a + size + 1);
 
 	procesador(size, nt);
@@ -224,34 +222,35 @@ int main()
 	//cout << size2;
 	size3 = size2;
 	temp2 = size3;
-	
-	while (temp <=nt)
+	validez=valides(vec, size);
+	while (temp <= nt && validez!=0)
 	{
 		while (size2 <= size)
 		{
 			merge_sort(vec, tam2, size2);
 			tam2 = size2;
 			size2 += size3;
-			
+
 		}
-		
+
 		temp++;
 		tam2 = 0;
 		size3 = size2;
-		size2 = temp2*temp;
-		cout << size2<< " ";
+		size2 = temp2 * temp;
+		//cout << size2 << " ";
 		if (size2 >= 1499)
 		{
 			temp = 0;
 			break;
 		}
-			
+
 		//size2 = size3;
 	}
-	if (temp == 0)
-		std::sort(vec.begin(), vec.begin() + size+ 1);
-	for (int i = 0; i < size - 2; i++)
-	cout << vec[i] << " ";
-	
-
+	if (temp == 0 && validez!=0)
+		std::sort(vec.begin(), vec.begin() + size + 1);
+	else {
+		merge_sort(vec, 0, size);
+	}
+	for (int i = 1; i <=size ; i++)
+		cout << vec[i] << " ";
 }
