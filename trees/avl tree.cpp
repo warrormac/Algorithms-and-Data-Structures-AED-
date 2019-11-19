526a025b3d0be5f3be73663cbb756c73eaf4a6e2
andre <andre.alvarez@ucsp.edu.pe>
2019-11-18 20:28:49 -0500

    Update avl tree.cpp

--- trees/avl tree.cpp
+++ trees/avl tree.cpp
@@ -1,11 +1,7 @@
-#include<iostream>
-#include<stdlib.h>
-#include <queue>
+#include<iostream> 
+#include <iomanip>
 using namespace std;
-
-
-// An AVL tree node
-
+  
 class Node
 {
 public:
@@ -15,9 +11,7 @@ public:
 	int height;
 };
 
-
 int max(int a, int b);
-
 
 int height(Node* N)
 {
@@ -33,29 +27,22 @@ int max(int a, int b)
 
 Node* newNode(int key)
 {
-
 	Node* node = new Node();
 	node->key = key;
 	node->left = NULL;
 	node->right = NULL;
-	node->height = 1; // new node is initially
-
+	node->height = 1; 
 	return(node);
 }
-
-
-// A utility function to right
-// rotate subtree rooted with y
-// See the diagram given above.
+  
 Node* rightRotate(Node* y)
 {
 	Node* x = y->left;
 	Node* T2 = x->right;
-
+ 
 	x->right = y;
 	y->left = T2;
-
-
+ 
 	y->height = max(height(y->left),
 		height(y->right)) + 1;
 	x->height = max(height(x->left),
@@ -64,10 +51,6 @@ Node* rightRotate(Node* y)
 	return x;
 }
 
-
-// A utility function to left
-// rotate subtree rooted with x
-// See the diagram given above.
 Node* leftRotate(Node* x)
 {
 	Node* y = x->right;
@@ -75,6 +58,7 @@ Node* leftRotate(Node* x)
 
 	y->left = x;
 	x->right = T2;
+
 	x->height = max(height(x->left),
 		height(x->right)) + 1;
 	y->height = max(height(y->left),
@@ -83,149 +67,173 @@ Node* leftRotate(Node* x)
 	return y;
 }
 
-
-// Get Balance factor of node N
-
 int getBalance(Node* N)
 {
-
 	if (N == NULL)
 		return 0;
 	return height(N->left) - height(N->right);
 }
-
+  
 Node* insert(Node* node, int key)
 {
-
 	if (node == NULL)
 		return(newNode(key));
 
 	if (key < node->key)
 		node->left = insert(node->left, key);
-
 	else if (key > node->key)
 		node->right = insert(node->right, key);
-
-	else // Equal keys are not allowed in BST
+	else
 		return node;
 
 	node->height = 1 + max(height(node->left),
 		height(node->right));
 
-
 	int balance = getBalance(node);
 
 	if (balance > 1 && key < node->left->key)
 		return rightRotate(node);
-	// Right Right Case
 
 	if (balance < -1 && key > node->right->key)
 		return leftRotate(node);
-	// Left Right Case
 
 	if (balance > 1 && key > node->left->key)
-
 	{
 		node->left = leftRotate(node->left);
 		return rightRotate(node);
 	}
-	// Right Left Case
 
 	if (balance < -1 && key < node->right->key)
-
 	{
 		node->right = rightRotate(node->right);
 		return leftRotate(node);
 	}
-	/* return the (unchanged) node pointer */
 	return node;
 }
-void printLevelOrder(Node* root)
+Node* minValueNode(Node* node)
 {
-	// Base Case
-	if (root == NULL) return;
+	Node* current = node;
 
-	// Create an empty queue for level order tarversal
-	queue<Node*> q;
+	while (current->left != NULL)
+		current = current->left;
 
-	// Enqueue Root and initialize height
-	q.push(root);
-
-	while (q.empty() == false)
-	{
-
-		int nodeCount = q.size();
-
-
-		while (nodeCount > 0)
-		{
-			Node* node = q.front();
-			cout << "\t" << node->key << " ";
-			q.pop();
-			if (node->left != NULL)
-				q.push(node->left);
-			if (node->right != NULL)
-				q.push(node->right);
-			nodeCount--;
-		}
-		cout << endl;
-	}
+	return current;
 }
+
+Node* deleteNode(Node* root, int key)
+{
+	if (root == NULL)
+		return root;
+
+	if (key < root->key)
+		root->left = deleteNode(root->left, key);
+
+	else if (key > root->key)
+		root->right = deleteNode(root->right, key);
+
+	else
+	{
+		if ((root->left == NULL) ||
+			(root->right == NULL))
+		{
+			Node* temp = root->left ?
+				root->left :
+				root->right;
+
+			if (temp == NULL)
+			{
+				temp = root;
+				root = NULL;
+			}
+			else
+				*root = *temp; 
+			free(temp);
+		}
+		else
+		{
+			Node* temp = minValueNode(root->right);
+			root->key = temp->key;
+			root->right = deleteNode(root->right,
+				temp->key);
+		}
+	}
+
+	if (root == NULL)
+		return root;
+
+	root->height = 1 + max(height(root->left),
+		height(root->right));
+
+	int balance = getBalance(root);
+
+	if (balance > 1 &&
+		getBalance(root->left) >= 0)
+		return rightRotate(root);
+
+	if (balance > 1 &&
+		getBalance(root->left) < 0)
+	{
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
+	}
+
+	if (balance < -1 &&
+		getBalance(root->right) <= 0)
+		return leftRotate(root);
+
+	if (balance < -1 &&
+		getBalance(root->right) > 0)
+	{
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+
+	return root;
+}
+
 void preOrder(Node* root)
 {
-
 	if (root != NULL)
-
 	{
-
-		printLevelOrder(root);
-		//preOrder(root->left);
-		//preOrder(root->right);
-
+		cout << root->key << " ";
+		preOrder(root->left);
+		preOrder(root->right);
 	}
 }
-
-
-// Driver Code
+void prints(Node* reference, int identation)
+{
+	if (!reference)
+		return;
+	prints(reference->left, identation + 4);
+	if (identation)
+		std::cout << std::setw(identation) << ' ';
+	std::cout << "" << reference->key << std::endl;
+	prints(reference->right, identation + 4);
+	return;
+}
 
 int main()
 {
-
 	Node* root = NULL;
 
+	root = insert(root, 4);
+	root = insert(root, 5);
+	root = insert(root, 6);
+	root = insert(root, 7);
+	root = insert(root, 2);
+	root = insert(root, 9);
 
 
-	/* Constructing tree given in
-
-	the above figure */
-
-	root = insert(root, 10);
-	root = insert(root, 20);
-	root = insert(root, 30);
-	root = insert(root, 40);
-	root = insert(root, 50);
-	root = insert(root, 25);
-	root = insert(root, 26);
-	root = insert(root, 90);
 	root = insert(root, 1);
+	root = insert(root, 8);
+	root = insert(root, 3);
 
+	prints(root, 0);
 
-	/* The constructed AVL Tree would be
+	system("pause");
+	cout << "\n\n\n\n\n\n";
 
-				30
-
-			/ \
-
-			20 40
-
-			/ \ \
-
-		10 25 50
-
-	*/
-
-	cout << "Preorder traversal of the "
-		"constructed AVL tree is \n";
-	preOrder(root);
+	root = deleteNode(root, 3);
+	prints(root, 0);
 	return 0;
 }
